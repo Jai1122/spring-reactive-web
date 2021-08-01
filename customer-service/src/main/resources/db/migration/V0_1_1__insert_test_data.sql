c620230e4de8f387eb3efcb0ac6be6b06e7c9172
Jai <jayapal1122@gmail.com>
2021-08-01 20:09:52 +0530

    Added Dockerfile and props

---
+++ customer-service/src/main/resources/db/migration/V0_1_1__insert_test_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO customer(created_at, email, "number", first_name, last_name) VALUES ( '2021-08-01 01:02:03' , 'test@example.com', '00306933177321', 'John', 'Doe')
--- otp-handler-service/src/test/java/com/jaiplays/services/otp/OtpHandlerServiceApplicationTests.java
+++ otp-handler-service/src/test/java/com/jaiplays/services/otp/OtpHandlerServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.jaiplays.services.otp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
